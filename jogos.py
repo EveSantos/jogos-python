e98015c8fdf19c41181005ac85d392b4da79181b
Evelyn Santos <66217438+EveSantos@users.noreply.github.com>
2021-01-06 18:15:30 -0300

    Add files via upload

---
+++ jogos.py
@@ -0,0 +1,10 @@
+
+print("*********************************")
+print("********Escolha seu jogo!********")
+print("*********************************")
+
+print("(1) Forca (2) Adivinhação")
+
+if(jogo == 1):
+    
+elif(jogo ==2):
