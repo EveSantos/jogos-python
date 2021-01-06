43d1928fd54ed6cf8caf00563990338d151aa354
Evelyn Santos <tardis@pop-os.localdomain>
2021-01-06 18:24:30 -0300

    Atualizando Jogos

--- jogos.py
+++ jogos.py
@@ -1,10 +1,19 @@
+# jogos.py
+
+import forca
+import adivinhacao
 
 print("*********************************")
-print("********Escolha seu jogo!********")
+print("*******Escolha o seu jogo!*******")
 print("*********************************")
 
 print("(1) Forca (2) Adivinhação")
 
-if(jogo == 1):
-    
-elif(jogo ==2):
+jogo = int(input("Qual jogo? "))
+
+if (jogo == 1):
+    print("Jogando forca")
+    forca.jogar()
+elif (jogo == 2):
+    print("Jogando adivinhação")
+    adivinhacao.jogar()
